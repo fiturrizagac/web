ca64a15603e49cb3a443228c65ea317837da9bf4
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-12-26 16:46:05 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     });
 });
 
-/* $(document).ready(function() {
+$(document).ready(function() {
     $.ajax({
         url: "http://52.141.219.120/products/api/products",
         headers: { 'Ocp-Apim-Subscription-Key': '9db32e4fed844af0aac4537ed2f784c8;product=unlimited' }
     }).then(function(data) {
        $('.productos').append(data[Math.floor(Math.random()*3)]);
     });
-}); */
+});
