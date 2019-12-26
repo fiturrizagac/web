e660a057db5377a3c883140f7739b7bfadbd8a09
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-12-26 16:49:31 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $.ajax({
-        url: "http://52.141.219.120/products/api/products",
-        headers: { 'Ocp-Apim-Subscription-Key': '9db32e4fed844af0aac4537ed2f784c8;product=unlimited' }
+        url: "http://52.151.237.69/v1/api/products",
+        headers: { 'Ocp-Apim-Subscription-Key': 'ea091aaf9fae4e3b8a7efd14e7949e74' }
     }).then(function(data) {
        $('.productos').append(data[Math.floor(Math.random()*3)]);
     });
